a823d71cc7687e4fa65c784d32aa2b97df2ff107
Landon West <landonwest815@gmail.com>
2023-01-13 23:28:36 -0700

    Created the basic Evaluator class with header comments.

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,24 @@
+﻿namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Author:    Landon West
+    /// Partner:   None
+    /// Date:      1/13/2023
+    /// Course:    CS 3500, University of Utah, School of Computing
+    /// Copyright: CS 3500 and Landon West - This work may not 
+    ///            be copied for use in Academic Coursework.
+    ///
+    /// I, Landon West, certify that I wrote this code from scratch and
+    /// did not copy it in part or whole from another source.  All 
+    /// references used in the completion of the assignments are cited 
+    /// in my README file.
+    ///
+    /// File Contents
+    ///
+    ///    This library class evaluates...
+    /// </summary>
+    public class Evaluator
+    {
+
+    }
+}
