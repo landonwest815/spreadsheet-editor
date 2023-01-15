c0c6648091a94758308a8c68b4938506bf5a1a28
Landon West <landonwest815@gmail.com>
2023-01-15 14:36:12 -0700

    Setup the testing method and made some small changes in the Evaluate class.

--- FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/Evaluator.cs
@@ -21,7 +21,7 @@ namespace FormulaEvaluator
     ///
     ///    This library class evaluates...
     /// </summary>
-    public class Evaluator
+    public static class Evaluator
     {
         // Delegate method to help find the value of a variable
         public delegate int Lookup(String variable_name);
@@ -46,13 +46,24 @@ namespace FormulaEvaluator
 
             foreach (String substring in substrings) {
 
+                Console.WriteLine(substring);
+
                 int number;
 
                 bool isVariable = false;
-                bool topIsMultiply = operators.Peek() == "*";
-                bool topIsDivide = operators.Peek() == "/";
-                bool topIsAdd = operators.Peek() == "+";
-                bool topIsSubtract = operators.Peek() == "-";
+
+                bool topIsMultiply = false;
+                bool topIsDivide = false;
+                bool topIsAdd = false;
+                bool topIsSubtract = false;
+
+                if (operators.Count > 0) 
+                { 
+                    topIsMultiply = operators.Peek() == "*";
+                    topIsDivide = operators.Peek() == "/";
+                    topIsAdd = operators.Peek() == "+";
+                    topIsSubtract = operators.Peek() == "-";
+                }
 
                 if (int.TryParse(substring, out number))
                 {
@@ -64,7 +75,7 @@ namespace FormulaEvaluator
                     else if (topIsDivide)
                     {
                         operators.Pop();
-                        values.Push(number / values.Pop());
+                        values.Push(values.Pop() / number);
                     }
                     else
                     {
@@ -104,7 +115,9 @@ namespace FormulaEvaluator
                     else if (topIsSubtract)
                     {
                         operators.Pop();
-                        values.Push(values.Pop() - values.Pop());
+                        int val1 = values.Pop();
+                        int val2 = values.Pop(); 
+                        values.Push(val2 - val1);
                     }
 
                     operators.Pop();
@@ -117,7 +130,9 @@ namespace FormulaEvaluator
                     else if (topIsDivide)
                     {
                         operators.Pop();
-                        values.Push(values.Pop() / values.Pop());
+                        int val1 = values.Pop();
+                        int val2 = values.Pop();
+                        values.Push(val2 / val1);
                     }
                 }
                 else
@@ -134,15 +149,18 @@ namespace FormulaEvaluator
             {
                 if (operators.Peek() == "+")
                 {
+                    operators.Pop();
                     return values.Pop() + values.Pop();
                 }
                 else if (operators.Peek() == "-")
                 {
-                    return values.Pop() - values.Pop();
+                    int val1 = values.Pop();
+                    int val2 = values.Pop();
+                    return val2 - val1;
                 }
                 else
                 {
-                    return 0;
+                    return 1;
                 }
             }
         }
--- FormulaEvaluatorTester/Program.cs
+++ FormulaEvaluatorTester/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FormulaEvaluator;
+
+public class FormulaEvaluatorTester
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(Evaluator.Evaluate("(6*(3+4))", null));
+    }
+}
